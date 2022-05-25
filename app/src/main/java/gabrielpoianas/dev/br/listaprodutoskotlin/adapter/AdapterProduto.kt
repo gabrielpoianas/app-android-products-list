b2b9af452116d5d0fc594955eded6f3b15bfdc98
gabrielpoianas <gpoianas@gmail.com>
2022-05-25 01:24:36 -0300

    Final commit

---
+++ app/src/main/java/gabrielpoianas/dev/br/listaprodutoskotlin/adapter/AdapterProduto.kt
@@ -0,0 +1,36 @@
+package gabrielpoianas.dev.br.listaprodutoskotlin.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import gabrielpoianas.dev.br.listaprodutoskotlin.R
+import gabrielpoianas.dev.br.listaprodutoskotlin.model.Produto
+
+class AdapterProduto(private val context: Context, private val produtos: MutableList<Produto>): RecyclerView.Adapter<AdapterProduto.ProdutoViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProdutoViewHolder {
+        val itemLista = LayoutInflater.from(context).inflate(R.layout.hardware_item, parent, false)
+        val holder = ProdutoViewHolder(itemLista)
+        return holder
+    }
+
+    override fun onBindViewHolder(holder: ProdutoViewHolder, position: Int) {
+        holder.foto.setImageResource(produtos[position].foto)
+        holder.nome.text = produtos[position].nome
+        holder.descricao.text = produtos[position].descricao
+        holder.preco.text = produtos[position].preco
+    }
+
+    override fun getItemCount(): Int = produtos.size
+
+    inner class ProdutoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val foto = itemView.findViewById<ImageView>(R.id.fotoProduto)
+        val nome = itemView.findViewById<TextView>(R.id.nomeProduto)
+        val descricao = itemView.findViewById<TextView>(R.id.descricaoProduto)
+        val preco = itemView.findViewById<TextView>(R.id.precoProduto)
+    }
+}
